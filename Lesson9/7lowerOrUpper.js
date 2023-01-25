bba1af074837b899e8fc04d26746751c82181b94
Dani Dimitrov <50717457+ddimitrov04@users.noreply.github.com>
2023-01-25 14:19:10 +0200

    Create 7lowerOrUpper.js

---
+++ Lesson9/7lowerOrUpper.js
@@ -0,0 +1,9 @@
+function sumNumber(a){
+    if (a == a.toUpperCase()) {
+        console.log('upper-case');
+       }
+    if (a == a.toLowerCase()){
+        console.log('lower-case');
+}
+}
+sumNumber('a');    
