df582ed23f03f15338c9fe652f8e8e9c976082a1
Dani Dimitrov <50717457+ddimitrov04@users.noreply.github.com>
2023-01-25 14:08:08 +0200

    Create 3townInfo.js

---
+++ Lesson9/3townInfo.js
@@ -0,0 +1,5 @@
+function sumNumber(townName,population,area){
+    console.log(`Town ${townName} has population of ${population} and area ${area} square km.`);
+ 
+}
+sumNumber('Sofia','123145213','492');    
