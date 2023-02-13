f62b050e7aee6383c0b566c7fa1a478c82b920fd
Dani Dimitrov <50717457+ddimitrov04@users.noreply.github.com>
2023-02-13 19:34:56 +0200

    Update 03MergeArrays.js

--- Lesson12/03MergeArrays.js
+++ Lesson12/03MergeArrays.js
@@ -1,29 +1,42 @@
-function commonElements(input1,input2){
-    let arr3=[];
-    for (let i = 0; i < input1.length; i++) {
-        if(i%2==0){
-           let number = parseInt(input1[i])+parseInt(input2[i]);
-           arr3[i] =number; 
-        }else{
-            let numberConcat = input1[i]+input2[i]
-            arr3[i] = numberConcat;
-        }
+// function commonElements(input1,input2){
+//     let arr3=[];
+//     for (let i = 0; i < input1.length; i++) {
+//         if(i%2==0){
+//            let number = parseInt(input1[i])+parseInt(input2[i]);
+//            arr3[i] =number; 
+//         }else{
+//             let numberConcat = input1[i]+input2[i]
+//             arr3[i] = numberConcat;
+//         }
         
-    }
-    // arr3.forEach(element => {
-    //     process.stdout.write(element + " - ")
-    // });
-    let finalPrint =``;
-    for (let i = 0; i < arr3.length; i++) {
-        if(i == arr3.length-1){
-            finalPrint+= arr3[i]
-        }else{
-            finalPrint+= `${arr3[i]} - `
-        }
+//     }
+//     // arr3.forEach(element => {
+//     //     process.stdout.write(element + " - ")
+//     // });
+//     let finalPrint =``;
+//     for (let i = 0; i < arr3.length; i++) {
+//         if(i == arr3.length-1){
+//             finalPrint+= arr3[i]
+//         }else{
+//             finalPrint+= `${arr3[i]} - `
+//         }
         
-    }
-    console.log(finalPrint);
+//     }
+//     console.log(finalPrint);
     
+// }
+// commonElements(['5', '15', '23', '56', '35'],['17', '22', '87', '36', '11']);
+// 80/100 Mistake 
+function mergeArrays(arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (i % 2 === 0) {
+      result.push(Number(arr1[i]) + Number(arr2[i]));
+    } else {
+      result.push(arr1[i] + arr2[i]);
+    }
+  }
+  console.log(result.join(" - "));
 }
-commonElements(['5', '15', '23', '56', '35'],['17', '22', '87', '36', '11']);
-//80/100 Mistake somewhere? :(
+mergeArrays(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11'])
